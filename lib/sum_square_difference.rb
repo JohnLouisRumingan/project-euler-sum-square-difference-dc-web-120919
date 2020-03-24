fc6508c14f6601dff4fe00ac2d14f6490cb07be1
John Louis Rumingan <jmrumingan@gmail.com>
2020-03-24 09:46:04 -0400

    Done.

--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,14 @@
-# Implement your procedural solution here!
+def sum_square_difference(num) 
+
+    sum_squares = 0 
+    array_sum = 0 
+  
+    for i in 1..num do
+  
+      sum_squares += (i**2)
+      array_sum += i
+    end 
+    square_sum = array_sum**2
+  
+    return (square_sum - sum_squares)
+  end 
